841e1e71cc4a1051901bf961931d5a68ca9a1538
Alex Asanov <asanov17.44@gmail.com>
2024-02-01 18:33:07 +0400

    added second task

--- Program.cs
+++ Program.cs
@@ -3,9 +3,26 @@
 // a = 256 => 26
 // a = 891 => 81
 
-int number = 947;
-int firstDigit = number / 100; // 453/100 = 4 целых (деление без остатка)
-int thirdDigit = number % 10; // 453 % 10 = 3 (остаток от деления на 10)
-int result = firstDigit * 10 + thirdDigit; // 4* 10 + 3 = 43
-// $"Текст {result} Текст {a+100} текст {a-500}"
-Console.WriteLine($"В числе {number} => {result}");
+// int number = 947;
+// int firstDigit = number / 100; // 453/100 = 4 целых (деление без остатка)
+// int thirdDigit = number % 10; // 453 % 10 = 3 (остаток от деления на 10)
+// int result = firstDigit * 10 + thirdDigit; // 4* 10 + 3 = 43
+// // $"Текст {result} Текст {a+100} текст {a-500}"
+// Console.WriteLine($"В числе {number} => {result}");
+
+// Напишите программу, которая принимает на вход трёхзначное
+// число и возводит вторую цифру этого числа в степень, равную
+// третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+
+ int number = 925;
+ int secondDigit = number / 10 % 10; // 947 / 10 % 10 = 94 % 10 = 4
+ int thirdDigit = number % 10; // 947 % 10 = 7 (Так мы находим 3ю цифру в 3х значном числе)
+ // Math.Pow (5, 2) => 5 ^ 2 = 5 * 5 = 25
+ int result = (int)Math.Pow(secondDigit, thirdDigit);
+ // Результат переводится в (int),
+  Console.WriteLine($"{secondDigit} ^ {thirdDigit} = {result}");
+  // 4 ^ 7 = 16384
